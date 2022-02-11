5850fb0da6572d778029176317143e1a4cca3426
Ednardo Gomes <ednardo@email.com>
2022-02-11 00:08:01 -0300

    Instalando axios e implementando a API da consulta do cep.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
+import { useState } from "react";
 import { MdSearch } from "react-icons/md";
+import { api } from "./services/api";
 
 import "./styles.css";
 
 export function App() {
+  const [input, setInput] = useState("");
+  const [address, setAddress] = useState({});
+
+  async function handleSearch() {
+    if (input === "") {
+      alert("Deve informar um cep para pesquisar.");
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      const data = response.data;
+
+      setInput("");
+      setAddress(data);
+    } catch {
+      alert("Erro na consultar do cep");
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Busca CEP</h1>
 
       <div className="containerInput">
-        <input type="text" placeholder="Digite o cep..." />
+        <input
+          type="text"
+          placeholder="Digite o cep..."
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <MdSearch size={25} color="#fff" />
         </button>
       </div>
 
       <main className="main">
-        <h2>CEP: 60000-000</h2>
+        <h2>CEP: {address.cep}</h2>
 
-        <span>Rua: descrição da rua</span>
-        <span>Complemento: descrição do complemento</span>
-        <span>Bairro: descrição do bairro</span>
-        <span>Fortalez - CE</span>
+        <span>Rua: {address.logradouro}</span>
+        <span>Complemento: {address.complemento}</span>
+        <span>Bairro: {address.bairro}</span>
+        <span>
+          {address.localidade} - {address.uf}
+        </span>
       </main>
     </div>
   );
